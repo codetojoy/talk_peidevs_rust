2a43ce81da0e7856a958ec2737787595a4339fe5
Michael Easter <codetojoy@gmail.com>
2021-02-05 09:10:46 -0400

    refactored with generics

--- src/typescript/src/animal.ts
+++ src/typescript/src/animal.ts
@@ -3,9 +3,9 @@ export interface Animal {
   getAge(): number;
 }
 
-export function oldest(x: Animal, y: Animal): Animal {
+export function oldest<T extends Animal>(x: T, y: T): T {
   console.log(`TRACER x: ${x.getNoise()}`);
   console.log(`TRACER y: ${y.getNoise()}`);
-  let result: Animal = x.getAge() > y.getAge() ? x : y;
+  let result: T = x.getAge() > y.getAge() ? x : y;
   return result;
 }
