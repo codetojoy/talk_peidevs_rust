b524519b639f2c96cdcfbc588ac358946a8f95ab
Michael Easter <codetojoy@gmail.com>
2021-02-10 14:47:44 -0400

    incremental update Wed 10 Feb 2021 14:47:44 AST

--- src/rust/cats_1_simple_sort/src/main.rs
+++ src/rust/cats_1_simple_sort/src/main.rs
@@ -1,16 +1,5 @@
 
-// goal: sort list of cats
-
 use std::vec::Vec;
-
-impl Cat {
-    pub fn new() -> Cat {
-        Cat {
-            name: String::from(""),
-            age: 0,
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct Cat {
@@ -20,9 +9,9 @@ pub struct Cat {
 
 fn main() {
     let mut cats = Vec::<Cat>::new();
-    let cat1 = Cat {name: String::from("Mozart"), age: 4, .. Cat::new()};
-    let cat2 = Cat {name: String::from("Chopin"), age: 3, .. Cat::new()};
-    let cat3 = Cat {name: String::from("Bach"), age: 5, .. Cat::new()};
+    let cat1 = Cat {name: String::from("Mozart"), age: 4};
+    let cat2 = Cat {name: String::from("Chopin"), age: 3};
+    let cat3 = Cat {name: String::from("Bach"), age: 5};
     cats.push(cat1);
     cats.push(cat2);
     cats.push(cat3);
